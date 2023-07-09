74deb40ffeaefa97c64f851c1b2e3b8fb9ce08e4
Izaac Zavaleta <izaac@rancher.com>
2023-07-09 13:14:43 -0700

    ssh helpers

--- jenkins/run.sh
+++ jenkins/run.sh
@@ -89,4 +89,5 @@ docker run --network container:rancher --name "${CYPRESS_CONTAINER_NAME}" -t \
   -w /e2e "cypress/${CYPRESS_DOCKER_TYPE}:${CYPRESS_DOCKER_VERSION}"
 
 sudo chown -R $(whoami) .
-jrm junit.xml "cypress/results/junit-*"
+find . -type f -iname junit-*
+jrm junit.xml "cypress/results/junit-*" "./junit-*"
