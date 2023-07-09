a1b3e8dafc8e244cf26e5e50efa2617e9d820ee6
Izaac Zavaleta <izaac@rancher.com>
2023-07-09 15:13:51 -0700

    golang deps

--- jenkins/generate_env.sh
+++ jenkins/generate_env.sh
@@ -10,6 +10,6 @@ if [ "false" != "${DEBUG}" ]; then
 fi
 
 PRIV_KEY="${HOME}/.ssh/jenkins_ecdsa"
-NODE_EXTERNAL_IP="$(corral vars ci single_ip)"
+NODE_EXTERNAL_IP="$(./corral vars ci single_ip)"
 scp -i ${PRIV_KEY} -o StrictHostKeyChecking=no \
   -o UserKnownHostsFile=/dev/null .env "${AWS_SSH_USER}@${NODE_EXTERNAL_IP}:/home/${AWS_SSH_USER}"
--- jenkins/sshexec.sh
+++ jenkins/sshexec.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PRIV_KEY="${HOME}/.ssh/jenkins_ecdsa"
-NODE_EXTERNAL_IP="$(corral vars ci single_ip)"
+NODE_EXTERNAL_IP="$(./corral vars ci single_ip)"
 
 ssh -i ${PRIV_KEY} -o StrictHostKeyChecking=no \
   -o UserKnownHostsFile=/dev/null "${AWS_SSH_USER}@${NODE_EXTERNAL_IP}" $1
